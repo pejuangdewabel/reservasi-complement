2f000dfee85deaef9afb4b898dd3f1343cf54f65
prasetyobella17 <prasetyo.bella@ancol.com>
2023-03-08 09:26:19 +0700

    update table responsive

--- resources/views/pages/user/history.blade.php
+++ resources/views/pages/user/history.blade.php
@@ -24,82 +24,84 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Data Riwayat Generate Tiket</h5>
-                            <table class="table datatable">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Jenis Tiket</th>
-                                        <th scope="col">Kode Jenis Tiket</th>
-                                        <th scope="col">Kode Tiket</th>
-                                        <th scope="col">Unit</th>
-                                        <th scope="col">Tiket</th>
-                                        <th scope="col">Tiket per orang</th>
-                                        <th scope="col">Tanggal Berlaku</th>
-                                        <th scope="col">Jenis Kendaraan</th>
-                                        <th scope="col">Jumlah Kendaraan</th>
-                                        <th scope="col">Pengunjung</th>
-                                        <th scope="col">Download Tiket</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($dataHistory as $dH)
+                            <div class="table-responsive">
+                                <table class="table datatable">
+                                    <thead>
                                         <tr>
-                                            <th scope="row">{{ $loop->iteration }}</th>
-                                            <th>
-                                                @if ($dH->relasi_tiket_jenis)
-                                                    {{ $dH->relasi_tiket_jenis->keterangan }}
-                                                @else
-                                                    ---
-                                                @endif
-                                            </th>
-                                            <td>
-                                                @foreach ($dH->jenisid as $key => $value)
-                                                    | {{ $value }}
-                                                @endforeach
-                                            </td>
-                                            <td>
-                                                <ul>
-                                                    @for ($i = 0; $i < count($dH->kode); $i++)
-                                                        <li>{{ $dH->kode[$i] }}</li>
-                                                    @endfor
-                                                </ul>
-                                            </td>
-                                            <td>
-                                                <ul>
-                                                    @for ($i = 0; $i < count($dH->unit); $i++)
-                                                        <li>{{ $dH->unit[$i] }}</li>
-                                                    @endfor
-                                                </ul>
-                                            </td>
-                                            <td>
-                                                {{ $dH->jumlah_tiket }}
-                                            </td>
-                                            <td>
-                                                {{ $dH->jumlah_org_per_tiket }}
-                                            </td>
-                                            <td>{{ tanggal_indonesia($dH->tgl_mulai) }} s/d
-                                                {{ tanggal_indonesia($dH->tgl_berlaku) }}</td>
-                                            <td>{{ $dH->kendaran ? $dH->kendaran : '---' }}</td>
-                                            <td>{{ $dH->jumlah_kendaraan_per_tiket ? $dH->jumlah_kendaraan_per_tiket : '---' }}
-                                            </td>
-                                            <td>
-                                                @if ($dH->guestName)
-                                                    {{ $dH->guestName }}
-                                                @else
-                                                    ---
-                                                @endif
-                                            </td>
-                                            <td>
-                                                <a href="{{ route('download-tiket-history', Crypt::encryptString($dH->id)) }}"
-                                                    class="btn btn-success btn-sm">
-                                                    DOWNLOAD TIKET
-                                                </a>
-                                            </td>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Jenis Tiket</th>
+                                            <th scope="col">Kode Jenis Tiket</th>
+                                            <th scope="col">Kode Tiket</th>
+                                            <th scope="col">Unit</th>
+                                            <th scope="col">Tiket</th>
+                                            <th scope="col">Tiket per orang</th>
+                                            <th scope="col">Tanggal Berlaku</th>
+                                            <th scope="col">Jenis Kendaraan</th>
+                                            <th scope="col">Jumlah Kendaraan</th>
+                                            <th scope="col">Pengunjung</th>
+                                            <th scope="col">Download Tiket</th>
                                         </tr>
-                                    @empty
-                                    @endforelse
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($dataHistory as $dH)
+                                            <tr>
+                                                <th scope="row">{{ $loop->iteration }}</th>
+                                                <th>
+                                                    @if ($dH->relasi_tiket_jenis)
+                                                        {{ $dH->relasi_tiket_jenis->keterangan }}
+                                                    @else
+                                                        ---
+                                                    @endif
+                                                </th>
+                                                <td>
+                                                    @foreach ($dH->jenisid as $key => $value)
+                                                        | {{ $value }}
+                                                    @endforeach
+                                                </td>
+                                                <td>
+                                                    <ul>
+                                                        @for ($i = 0; $i < count($dH->kode); $i++)
+                                                            <li>{{ $dH->kode[$i] }}</li>
+                                                        @endfor
+                                                    </ul>
+                                                </td>
+                                                <td>
+                                                    <ul>
+                                                        @for ($i = 0; $i < count($dH->unit); $i++)
+                                                            <li>{{ $dH->unit[$i] }}</li>
+                                                        @endfor
+                                                    </ul>
+                                                </td>
+                                                <td>
+                                                    {{ $dH->jumlah_tiket }}
+                                                </td>
+                                                <td>
+                                                    {{ $dH->jumlah_org_per_tiket }}
+                                                </td>
+                                                <td>{{ tanggal_indonesia($dH->tgl_mulai) }} s/d
+                                                    {{ tanggal_indonesia($dH->tgl_berlaku) }}</td>
+                                                <td>{{ $dH->kendaran ? $dH->kendaran : '---' }}</td>
+                                                <td>{{ $dH->jumlah_kendaraan_per_tiket ? $dH->jumlah_kendaraan_per_tiket : '---' }}
+                                                </td>
+                                                <td>
+                                                    @if ($dH->guestName)
+                                                        {{ $dH->guestName }}
+                                                    @else
+                                                        ---
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    <a href="{{ route('download-tiket-history', Crypt::encryptString($dH->id)) }}"
+                                                        class="btn btn-success btn-sm">
+                                                        DOWNLOAD TIKET
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
